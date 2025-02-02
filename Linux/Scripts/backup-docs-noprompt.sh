01d4a7dff7d7ab14390677db7b5d000314c0ea1b
Thomas <106869856+tlarson91119@users.noreply.github.com>
2025-02-02 00:43:36 -0600

    Update backup-docs-noprompt.sh

--- Linux/Scripts/backup-docs-noprompt.sh
+++ Linux/Scripts/backup-docs-noprompt.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-USER_NAME=user
-SERVER_NAME=server
 
 if ssh USER_NAME@SERVER_NAME '[ -d "/mnt/RAID5" ]'; then
  echo "Directory found"
  logger Backup script started
- rsync -Pau --delete /home/$USER_NAME/Documents/$USER_NAME@$SERVER_NAME:/mnt/RAID5/$USER_NAME/Documents/
+ rsync -Pau --delete /home/username/Documents/ username@server:/mnt/RAID5/username/Documents/
 else
  echo "Could not connect to the server (or find RAID5 directory)..."
  logger -p user.err Backup script failed to find destination
